25850ebfba9021237b0a1ab429bd449e4c6a9bc6
AynurGaliev <aynur.galiev@flatstack.com>
2016-01-31 01:50:57 +0300

    Initial commit

---
+++ Shimmer/BridgeHeader.h
@@ -0,0 +1,9 @@
+//
+//  BridgeHeader.h
+//  Shimmer
+//
+//  Created by Galiev Aynur on 31.01.16.
+//  Copyright © 2016 FlatStack. All rights reserved.
+//
+
+#import "FBShimmeringView.h"
--- Shimmer/ViewController.swift
+++ Shimmer/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shimmerView: FBShimmeringView!
+    
+    override func loadView() {
+        super.loadView()
+        let label = UILabel()
+        label.frame = self.shimmerView.bounds
+        label.text = "Some Text"
+        label.font = UIFont.systemFontOfSize(50.0, weight: UIFontWeightUltraLight)
+        label.textAlignment = NSTextAlignment.Center
+        label.textColor = UIColor.whiteColor()
+        shimmerView.contentView = label
+        shimmerView.shimmeringHighlightLength = 0.7
+        shimmerView.shimmeringOpacity = 0.9
+        shimmerView.shimmeringSpeed = 150
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.shimmerView.shimmering = true
     }
 
     override func didReceiveMemoryWarning() {
